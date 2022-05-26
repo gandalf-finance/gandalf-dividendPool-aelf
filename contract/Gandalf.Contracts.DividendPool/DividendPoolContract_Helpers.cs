b8cf9b8ef2e3dcb0d0c866ece5cf823e586991e0
weili <woshilw0416@gmail.com>
2022-05-26 14:50:25 +0800

    modify GetRewardDebt && GetAccPerShare

--- contract/Gandalf.Contracts.DividendPool/DividendPoolContract_Helpers.cs
+++ contract/Gandalf.Contracts.DividendPool/DividendPoolContract_Helpers.cs
@@ -7,22 +7,18 @@ namespace Gandalf.Contracts.DividendPoolContract
         private BigIntValue GetAccPerShare(long pid, string token, bool isInitialIfNull = true)
         {
             var accPerShare = State.AccPerShare[pid][token];
-            if (accPerShare == null && isInitialIfNull)
-            {
-                State.AccPerShare[pid][token] = new BigIntValue(0);
-            }
-
+            if (accPerShare != null || !isInitialIfNull) return accPerShare;
+            accPerShare = new BigIntValue(0);
+            State.AccPerShare[pid][token] = accPerShare;
             return accPerShare;
         }
 
         private BigIntValue GetRewardDebt(long pid, Address address, string token, bool isInitialIfNull = true)
         {
             var rewardDebt = State.RewardDebt[pid][address][token];
-            if (rewardDebt == null && isInitialIfNull)
-            {
-                State.RewardDebt[pid][address][token] = new BigIntValue(0);
-            }
-
+            if (rewardDebt != null || !isInitialIfNull) return rewardDebt;
+            rewardDebt = new BigIntValue(0);
+            State.RewardDebt[pid][address][token] = rewardDebt;
             return rewardDebt;
         }
     }
